3c0f444106d0cd2f5cc400c6007d18066a51f4e3
Marina Chompalova <Marina.Chompalova@Ezbob.local>
2018-03-23 23:45:08 +0200

    fixed msg.reply()

--- src/Consumer.java
+++ src/Consumer.java
@@ -14,13 +14,14 @@ public class Consumer extends AbstractVerticle{
         //eventBus.consumer(Constants.ADDRESS, msg -> System.out.println(new Timestamp(System.currentTimeMillis()) + ": " + "A message received: " + msg.body()));
         eventBus.consumer(Constants.ADDRESS, msg -> {
             System.out.println("A message received: " + msg.body());
-            msg.reply("Hi from consumer." , res -> {
+            /*msg.reply("Hi from consumer." , res -> {
                 if (res.succeeded()) {
                     System.out.println("Consumer: Successfully sent reply");
                 } else {
                     System.out.println("Consumer: Failed to send reply." + res.cause());
                 }
-            });
+            });*/
+            msg.reply("ACK from Consumer.");
         });
     }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -10,12 +10,11 @@ public class Sender extends AbstractVerticle {
         //vertx.setPeriodic(5000, res -> eventBus.publish(Constants.ADDRESS,"From sender: flooding the network."));
         eventBus.send(Constants.ADDRESS, "Hello from sender", res -> {
             if (res.succeeded()) {
-                System.out.println("Sender: Successfully sent msg");
+                System.out.println("Sender: Received a reply: " + res.result().body());
             } else {
-                System.out.println("Sender: Failed to send msg." + res.cause());
+                System.out.println("Sender: Failed to receive a reply." + res.cause());
             }
         });
-        //eventBus.consumer(Constants.ADDRESS, msg -> System.out.println("received msg from consumer:" + msg.body()));
     }
 
     @Override
