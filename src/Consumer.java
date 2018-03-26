00798060e1cc9f3ae45274e1015880f6c5158ca2
Marina Chompalova <Marina.Chompalova@Ezbob.local>
2018-03-26 10:37:41 +0300

    minor amends

--- src/Consumer.java
+++ src/Consumer.java
@@ -5,22 +5,13 @@ import org.jruby.RubyProcess;
 import java.sql.Timestamp;
 
 public class Consumer extends AbstractVerticle{
-    protected EventBus eventBus = null;
-    final public static String NAME = "Consumer";
+    final public static String NAME = "CONSUMER";
 
     @Override
     public void start() {
-        eventBus = vertx.eventBus();
-        //eventBus.consumer(Constants.ADDRESS, msg -> System.out.println(new Timestamp(System.currentTimeMillis()) + ": " + "A message received: " + msg.body()));
+        final EventBus eventBus = vertx.eventBus();
         eventBus.consumer(Constants.ADDRESS, msg -> {
             System.out.println("A message received: " + msg.body());
-            /*msg.reply("Hi from consumer." , res -> {
-                if (res.succeeded()) {
-                    System.out.println("Consumer: Successfully sent reply");
-                } else {
-                    System.out.println("Consumer: Failed to send reply." + res.cause());
-                }
-            });*/
             msg.reply("ACK from Consumer.");
         });
     }
--- src/Sender.java
+++ src/Sender.java
@@ -2,15 +2,14 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 
 public class Sender extends AbstractVerticle {
-    final public static String NAME = "Sender";
+    final public static String NAME = "SENDER";
 
     @Override
     public void start() {
-        EventBus eventBus = vertx.eventBus();
-        //vertx.setPeriodic(5000, res -> eventBus.publish(Constants.ADDRESS,"From sender: flooding the network."));
-        eventBus.send(Constants.ADDRESS, "Hello from sender", res -> {
+        final EventBus eventBus = vertx.eventBus();
+        eventBus.send(Constants.ADDRESS, "Hello from sender.", res -> {
             if (res.succeeded()) {
-                System.out.println("Sender: Received a reply: " + res.result().body());
+                System.out.println("Sender: received a reply: " + res.result().body());
             } else {
                 System.out.println("Sender: Failed to receive a reply." + res.cause());
             }
